2e74a6574f78af8ebc0aa3c1fa2d9a1f545fea12
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2017-12-20 23:47:41 -0800

    set up a simple webhook to test, works

--- scripts/automod.js
+++ scripts/automod.js
@@ -1,5 +1,24 @@
+require('dotenv').config();
+
+function verifyIncomingWebhook(req, res, next) {
+  const verificationToken = process.env.SLACK_VERIFICATION_TOKEN;
+
+  if (req.body.token === verificationToken) {
+    next();
+  }
+  else {
+    res.sendStatus(403);
+  }
+}
+
 module.exports = (robot) => {
   robot.respond(/hi|hello|howdy/i, (res) => {
     res.send('Good day to ya!');
+  });
+
+  robot.router.post('/automod/discuss', verifyIncomingWebhook, (req, res) => {
+    console.log(req.body);
+
+    res.send('Creating private room to discuss.');
   });
 };
