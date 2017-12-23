1d598f65037a73855d20c7dd629b3ad998e7a638
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2017-12-23 04:51:43 -0800

    pull in middleware instead of writing it in the file

--- scripts/automod.js
+++ scripts/automod.js
@@ -1,24 +1,16 @@
 require('dotenv').config();
 
-function verifyIncomingWebhook(req, res, next) {
-  const verificationToken = process.env.SLACK_VERIFICATION_TOKEN;
-
-  if (req.body.token === verificationToken) {
-    next();
-  }
-  else {
-    res.sendStatus(403);
-  }
-}
+const wrap = require('../support/wrapAsync');
+const verifyIncomingWebhook = require('../support/verifyWebhook');
 
 module.exports = (robot) => {
   robot.respond(/hi|hello|howdy/i, (res) => {
     res.send('Good day to ya!');
   });
 
-  robot.router.post('/automod/discuss', verifyIncomingWebhook, (req, res) => {
+  robot.router.post('/automod/discuss', verifyIncomingWebhook, wrap((req, res) => {
     console.log(req.body);
 
     res.send('Creating private room to discuss.');
-  });
+  }));
 };
